21118c068468adecaf27afc22df3b80ce1cc3a5e
Nikhil Motwani <nmotwani@seas.upenn.edu>
2020-05-03 19:26:47 -0400

    Basic Recommendation UI created

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
@@ -9,22 +9,18 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import SideBar from './SideBar';
-// import Chart from './Chart';
-// import Deposits from './Deposits';
-// import Orders from './Orders';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import {useSelector} from 'react-redux';
 
 const drawerWidth = 240;
 
@@ -105,18 +101,216 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     height: 240,
   },
+  tabs: {
+    backgroundColor: theme.palette.background.paper,
+    width: '100%',
+  }, 
+  card: {
+    maxWidth: '50',
+    height: '100%',
+    "&:hover": {
+      boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+    }
+  },
+  cardMedia: {
+  //  height: '75%', 
+  height: 0,
+  paddingTop: '87.25%', // 16:9
+  },
+  cardContent: {
+    height: '20%',
+    overflow: 'auto',
+    '&::-webkit-scrollbar': {
+      width: '0.4em'
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+      webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      outline: '1px solid slategrey'
+    }
+  },
 }));
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+var ratings = [];
 
 export default function Recommendation() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
+  const [value, setValue] = React.useState(0);
+  const [selectedrating, setRating] = React.useState(0);
+
+  const theme = useTheme();
+  const auth = useSelector(state => state.auth);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);  
+
+  // Function when view is clicked
+  const handleListItemClick = (book, index) => {
+    // PopUp book details
+  };
+
+  const handleLikedBooks = (event, isbn) => {
+    const button = event.target;
+ 
+    if(button.innerHTML === 'Like') {
+      // fetch call for Like make likeFlag: 1
+      fetch('http://localhost:8081/api/user/likeBook/'+isbn, {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'    
+         },
+         body:JSON.stringify({email: auth.auth, id:1})
+       })
+       .then(resp => resp.json())
+       .then(resp => {
+         if(resp.message === 'success') {
+           button.innerHTML = 'UnLike';
+         }
+       });
+    }else if(button.innerHTML === 'UnLike') {
+      // fetch call for Unlike make likeFlag: 0
+      fetch('http://localhost:8081/api/user/likeBook/'+isbn, {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'    
+         },
+         body:JSON.stringify({email: auth.auth, id:0})
+       })
+       .then(resp => resp.json())
+       .then(resp => {
+         if(resp.message === 'success') {
+           button.innerHTML = 'Like';
+         }
+       });
+    }
+   }
+ 
+   const handleReadBooks = (event, isbn) => {
+     const button = event.target;
+ 
+     if(button.innerHTML === 'Mark Read') {
+       // fetch call for Read make readFlag: 1
+       fetch('http://localhost:8081/api/user/readBook/'+isbn, {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'    
+         },
+         body:JSON.stringify({email: auth.auth, id:1})
+       })
+       .then(resp => resp.json())
+       .then(resp => {
+         if(resp.message === 'success') {
+           button.innerHTML = 'Mark UnRead';
+         }
+       });
+ 
+     }else if(button.innerHTML === 'Mark UnRead') {
+       // fetch call for UnRead make readFlag: 0
+       fetch('http://localhost:8081/api/user/readBook/'+isbn, {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'    
+         },
+         body:JSON.stringify({email: auth.auth, id:0})
+       })
+       .then(resp => resp.json())
+       .then(resp => {
+         if(resp.message === 'success') {
+           button.innerHTML = 'Mark Read';
+         }
+       });
+     }
+   }
+
+
+  const showrecommendation1 = () => {
+    const email = auth.auth;
+
+    // Fetch Call
+
+    let recdiv =  < Grid item key={1} xs={3} style={{ height: '400px', width: '180px' }}>
+            <Card className={classes.card}>
+              <CardMedia className ={classes.cardMedia}
+                image={'https://s1.nyt.com/du/books/images/9781476746586.jpg'}
+              />
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h4">
+                TITLE PLACE HOLDER
+                </Typography>
+              </CardContent>
+              <CardActions style={{width:'100%'}}>
+              <Button size="medium" color="textSecondary" onClick = {() => handleListItemClick('', '')}>
+                  View
+              </Button>
+              <div style={{width: '100%', textAlign:'right'}}>  
+              <Button color="secondary" onClick={(event) => {handleLikedBooks(event, 'put isbn here')}}>
+               Like 
+              </Button>
+
+              <Button color="primary" onClick={(event) => {handleReadBooks(event, 'put isbn here')}}>
+                Mark read
+              </Button>
+              </div>       
+              </CardActions>
+            </Card>
+        </Grid >
+
+  }
 
   return (
     <div className={classes.root}>
@@ -124,31 +318,113 @@ export default function Recommendation() {
       <SideBar name='Recommended Books'/>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                {/* <Chart /> */}
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                {/* <Deposits /> */}
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                {/* <Orders /> */}
-              </Paper>
-            </Grid>
+        <div className={classes.tabs}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Recommendation 1" {...a11yProps(0)} />
+          <Tab label="Recommendation 2" {...a11yProps(1)} />
+          <Tab label="Recommendation 3" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+
+        {/* Recommendation 1 */}
+        <TabPanel value={value} index={0} dir={theme.direction}>
+      
+          <Grid container spacing={2}> 
+
+          {/* Book Cards */}
+          {/* Call showrecommendation1 here */}
+          < Grid item key={1} xs={3} style={{ height: '400px', width: '180px' }}>
+            <Card className={classes.card}>
+              <CardMedia className ={classes.cardMedia}
+                image={'https://s1.nyt.com/du/books/images/9781476746586.jpg'}
+              />
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h4">
+                TITLE PLACE HOLDER
+                </Typography>
+              </CardContent>
+              <CardActions style={{width:'100%'}}>
+              <Button size="medium" color="textSecondary">
+                  View
+              </Button>
+              <div style={{width: '100%', textAlign:'right'}}>  
+              <Button color="secondary" onClick={(event) => {handleLikedBooks(event, 'put isbn here')}}>
+               Like 
+              </Button>
+
+              <Button color="primary" onClick={(event) => {handleReadBooks(event, 'put isbn here')}}>
+                Mark read
+              </Button>
+              </div>              
+              </CardActions>
+            </Card>
+        </Grid >
+
+        < Grid item key={1} xs={3} style={{ height: '400px', width: '180px' }}>
+            <Card className={classes.card}>
+              <CardMedia className ={classes.cardMedia}
+                image={'https://s1.nyt.com/du/books/images/9781476746586.jpg'}
+              />
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h4">
+                TITLE PLACE HOLDER
+                </Typography>
+              </CardContent>
+              <CardActions style={{width:'100%'}}>
+              <Button size="medium" color="textSecondary">
+                  View
+              </Button>
+              <div style={{width: '100%', textAlign:'right'}}>  
+              <Button color="secondary" onClick={(event) => {handleLikedBooks(event, 'put isbn here')}}>
+               Like 
+              </Button>
+
+              <Button color="primary" onClick={(event) => {handleReadBooks(event, 'put isbn here')}}>
+                Mark read
+              </Button>
+              </div>       
+              </CardActions>
+            </Card>
+          </Grid >
           </Grid>
-          <Box pt={4}>
-            <Copyright />
-          </Box>
-        </Container>
+        </TabPanel>
+
+        {/* Recommendation 2 */}
+        <TabPanel value={value} index={1} dir={theme.direction}>    
+        <Grid container spacing={2}> 
+
+         {/* Book Cards */}
+         {/* Call showrecommendation2 here */}
+        </Grid>      
+        </TabPanel>
+
+
+
+        {/* Recommendation 3 */}
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          <Grid container spacing={2}> 
+
+            {/* Book Cards */}
+            {/* Call showrecommendation3 here */}
+          
+        
+           </Grid>          
+        </TabPanel>        
+      </SwipeableViews>
+    </div>        
       </main>
     </div>
   );
