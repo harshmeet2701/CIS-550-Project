e71c1f512dd89b42495bd96301f8d5ffeb21fb78
Harshmeet Singh <53871103+harshmeet2701@users.noreply.github.com>
2020-05-03 20:51:02 -0400

    Pop Up Support for Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,10 +15,18 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { useState, useEffect } from 'react';
-
-// import Chart from './Chart';
-// import Deposits from './Deposits';
-// import Orders from './Orders';
+import { useSelector } from 'react-redux';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableRow from '@material-ui/core/TableRow';
+import Toolbar from '@material-ui/core/Toolbar';
+import CloseIcon from '@material-ui/icons/Close';
+import Table from '@material-ui/core/Table';
+import Slide from '@material-ui/core/Slide';
+import Rating from '@material-ui/lab/Rating';
 
 function Copyright() {
   return (
@@ -121,12 +129,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+var ratings = [];
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function Dashboard() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const [books, setBooks] = React.useState([]);
   const [likedbooks, setLikedBooks] = React.useState([]);
+  const auth = useSelector(state => state.auth);
+  const [selectedBook, setSelBook] = useState("");
+  const [index, setIndex] = useState(-1);
+  const [selectedrating, setRating] = useState(0);
+  const [openDailogue, setOpenDailogue] = React.useState(false);
+  const [authors, setAuthors] = useState([]);
+
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -140,6 +160,63 @@ export default function Dashboard() {
     getReadBooks();
     getLikedBooks();
   })
+
+  const handleCloseDailog = () => {
+    saveRating(selectedBook[0]);
+    setOpenDailogue(false);
+  }
+
+  const getAuthors = (isbn) => {
+
+    fetch('http://localhost:8081/api/book/getAuthors/' + isbn, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(resp => resp.json())
+      .then(authorList => {
+        console.log('getAuthors: ', authorList);
+        if (!authorList) return;
+
+        if (!authorList.rows) {
+          setAuthors([]);
+        } else {
+          setAuthors(authorList.rows);
+        }
+      })
+  }
+
+  const handleListItemClick = (authBookItem, index) => {
+    getAuthors(authBookItem[0]);
+    setSelBook(authBookItem);
+    setIndex(index);
+    setRating(ratings[index]);
+    setOpenDailogue(true);
+  };
+
+  const saveRating = (isbn) => {
+    console.log('Sel Rating:', selectedrating);
+    ratings[index] = selectedrating;
+    console.log('Ratings:', ratings[index]);
+
+    fetch('http://localhost:8081/api/user/rateBook/' + isbn, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: auth.auth, rating: selectedrating })
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        if (resp.message === 'success') {
+          console.log('Rating Updated');
+        }
+      });
+  }
+
 
   // Add code to display the to read and liked book
   function getReadBooks() {
@@ -156,8 +233,8 @@ export default function Dashboard() {
         if (!readBookList) return;
         console.log(readBookList);
 
-        let readBookDivs = readBookList.rows.map(readBookItem => (
-          <GridListTile key={readBookItem[0]} style={{ height: '240px', width: '160px' }}>
+        let readBookDivs = readBookList.rows.map((readBookItem, i) => (
+          <GridListTile item key={i} style={{ height: '240px', width: '160px' }} onClick={() => handleListItemClick(readBookItem, i)}>
             <img src={readBookItem[2] === null ? 'https://i.imgur.com/sJ3CT4V.gif' : readBookItem[2]} alt={readBookItem[1]} style={{ height: '240px', width: '160px' }} />
             <GridListTileBar
               title={readBookItem[1]}
@@ -196,8 +273,8 @@ export default function Dashboard() {
         if (!pubBookList) return;
         console.log(pubBookList);
 
-        let likedBookDivs = pubBookList.rows.map(likedBookItem => (
-          <GridListTile key={likedBookItem[0]} style={{ height: '240px', width: '160px' }}>
+        let likedBookDivs = pubBookList.rows.map((likedBookItem, i) => (
+          <GridListTile item key={i} style={{ height: '240px', width: '160px' }} onClick={() => handleListItemClick(likedBookItem, i)}>
             <img src={likedBookItem[2] === null ? 'https://i.imgur.com/sJ3CT4V.gif' : likedBookItem[2]} alt={likedBookItem[1]} style={{ height: '240px', width: '160px' }} />
             <GridListTileBar
               title={likedBookItem[1]}
@@ -243,6 +320,198 @@ export default function Dashboard() {
               {likedbooks}
             </GridList>
           </Grid>
+
+          <Dialog fullScreen open={openDailogue} onClose={handleCloseDailog} TransitionComponent={Transition}>
+            <AppBar className={classes.appBar}>
+              <Toolbar>
+                <IconButton edge="start" color="inherit" onClick={handleCloseDailog} aria-label="close">
+                  <CloseIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  Book Details
+              </Typography>
+              </Toolbar>
+            </AppBar>
+            <Container maxWidth="lg">
+              <Grid container xs={12} spacing={2} className={classes.container} >
+                <Grid item xs={4} style={{ display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
+                  <div>
+                    {/* <img src= {'https://i.imgur.com/sJ3CT4V.gif'} style= {{width: "180%", objectFit: "contain",  boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)"}} /> */}
+                    <a href={selectedBook[2] ? selectedBook[2] : 'https://i.imgur.com/sJ3CT4V.gif'} target={'_blank'}><img alt={'Book'} src={selectedBook[2] ? selectedBook[2] : 'https://i.imgur.com/sJ3CT4V.gif'} style={{ width: "100%", objectFit: "contain", boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)" }} /></a>
+                  </div>
+                </Grid>
+
+                <Grid item xs={8} style={{ display: 'flex', marginTop: '5%' }}>
+                  <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                      <TableBody>
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              ISBN:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" style={{ width: '15rem' }}>
+                            <Typography variant="h6">
+                              {selectedBook[0]}
+                            </Typography>
+                          </TableCell>
+
+                          <TableCell align="left">
+                            <Typography variant="h5" component="h1">
+                              Rating:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h6">
+                              {selectedBook[8]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              Title:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h7">
+                              {selectedBook[1]}
+                            </Typography>
+                          </TableCell>
+
+                          <TableCell align="left">
+                            <Typography variant="h5" component="h1">
+                              Pages:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h6">
+                              {selectedBook[9]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              Description
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h7">
+                              {selectedBook[3]}
+                            </Typography>
+                          </TableCell>
+
+                          <TableCell align="left">
+                            <Typography variant="h5" component="h1">
+                              Language:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h6">
+                              {selectedBook[10]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              URL
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h9">
+                              <a href={selectedBook[4] ? selectedBook[4] : ''} target={'_blank'}>{selectedBook[4]}</a>
+                            </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            <Typography variant="h6">
+                              {selectedBook[11]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              Authors:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+
+                            <Table aria-label="simple table">
+                              {authors.map((author, i) => {
+                                return (
+                                  <TableRow>
+                                    <TableCell align="left" >
+                                      <Typography variant="h7">
+                                        {author.WIKIURL ? <a href={author.WIKIURL} target={'_blank'}> {author.AUTHORNAME} </a> : author.AUTHORNAME}
+                                      </Typography>
+                                    </TableCell>
+                                  </TableRow>
+                                )
+                              })}
+                            </Table>
+
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              Publisher:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            {/* Selected Publisher */}
+                            <Typography variant="h7">
+                              {selectedBook[5]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '12rem' }}>
+                            <Typography variant="h5" component="h1">
+                              Publication Place:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            {/* Selected Place */}
+                            <Typography variant="h7">
+                              {selectedBook[6]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell align="left" style={{ width: '5em' }}>
+                            <Typography variant="h5" component="h1">
+                              Publication Date:
+                    </Typography>
+                          </TableCell>
+                          <TableCell align="left" >
+                            {/* Selected ISBN */}
+                            <Typography variant="h6">
+                              {selectedBook[7]}
+                            </Typography>
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Grid>
+              </Grid>
+            </Container>
+          </Dialog>
+          <Box pt={4}>
+            <Copyright />
+          </Box>
         </Container>
       </main>
     </div>
